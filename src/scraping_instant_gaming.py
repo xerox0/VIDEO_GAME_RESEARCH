59d2c6ca7e2499d01122261c061adc1d81b07b2c
LMarco <lupis.marco@yahoo.it>
2021-12-21 19:29:13 +0100

    Aggiunta di sviluppatore e pubblicatore. File csv trasformato in txt

--- src/scraping_instant_gaming.py
+++ src/scraping_instant_gaming.py
@@ -8,22 +8,37 @@ def start_soup(link):
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
 
-f = open('giochi.csv', 'a')
 
-for x in range(1, 5):  # 5 solo per testare, poi bisogna aggiungerne altre
+f = open('../database/giochi.txt', 'w')
+
+for x in range(1, 6):  # 5 solo per testare, poi bisogna aggiungerne altre
     base_link = f"https://www.instant-gaming.com/it/ricerca/?page={x}"
-    print(f'Scansiono la pag n.{x}' )
-    game_tag = start_soup(base_link).find_all('a', class_='cover') # è il tag 'a' che contiene il link della pagina del gioco
+    print(f'Scansiono la pag n.{x}')
+    game_tag = start_soup(base_link).find_all('a',class_='cover')  # è il tag 'a' che contiene il link della pagina del gioco
     for tag in game_tag:
         game_link = tag.get('href')  # estraggo il link della pagina del singolo gioco, per fare lo scraping della pagina
         game_soup = start_soup(game_link)
-        game_title = game_soup.find('h1').string # estrae il titolo del gioco dal tag
+        game_title = game_soup.find('h1').string  # estrae il titolo del gioco dal tag
         game_description = game_soup.find('div', itemprop='description')
+        # ci sono pagine senza descrizione. In tal caso vado avanti
         if game_description is None:
             continue
-        f.write(f'{game_title},{game_description}\n')
+
+        try:
+            game_developer = game_soup.find('a', content='Developer').string.replace('\n', ' ')
+            print(game_developer)
+            game_publisher = game_soup.find('a', content='Publisher').string.replace('\n', ' ')
+            print(game_publisher)
+        except:
+            continue
+
         # print(game_title)
-        # for string in game_description.stripped_strings: #  estrae la descrizione
-        #     print(string)
+        f.write(f'{game_title}\n '
+                f'Sviluppato da: {game_developer}\n '
+                f'Pubblicato da: {game_publisher}\n ')
+        for string in game_description.stripped_strings:  # estrae la descrizione, senza i tag html
+            f.write(string)
+        f.write('\n\n')
+
 f.close()
-  # requests.exceptions.TooManyRedirects: Exceeded 30 redirects. Troppe richieste ?
+# requests.exceptions.TooManyRedirects: Exceeded 30 redirects. Troppe richieste ?
