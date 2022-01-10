3dadf8db64b2ddf07342ac8ef1db47249e5797ac
toore <toore@Debian.home>
2022-01-10 12:47:05 +0100

    Modifiche al file di Scraping salvatore

--- src/ScrapingIterate.py
+++ src/ScrapingIterate.py
@@ -1,6 +1,7 @@
 import bs4
 import  requests
 import  nltk
+import re
 nltk.download('punkt')
 
 base_url = 'https://multiplayer.it'
@@ -18,20 +19,32 @@ for x in range (1,3): #il numero del range è 3327 per comodità testiamo su 2
 #test_link = 'https://multiplayer.it/giochi/mario-galaxy-2-per-wii.html'
 videogames_names = []
 videogames_description = []
+videogames_dev = []
+videogames_pub = []
 for link in videogames_links:
     r = requests.get(link) #, headers=headers)
     soup = bs4.BeautifulSoup(r.content, 'html.parser')
-
     name_and_console = soup.find('h1', class_='gamecard-title font-weight-bold ls-n1 lh-1 mb-3 mb-md-1 position-relative').text.strip('\n').strip('\n')
     description = soup.find('div', class_='py-3').text.rstrip('\n').strip('\n')
+
+    views = soup.findAll("a", class_="d-inline-block pb-3 pb-md-0 pr-3 pr-md-0 font-weight-bold")
+    view='d-inline-block pb-3 pb-md-0 pr-3 pr-md-0 font-weight-bold'
+    views2 = soup.findAll("a", class_="d-inline-block pb-3 pb-md-0 pr-3 pr-md-0 font-weight-bold")
+    view2='d-inline-block pb-3 pb-md-0 pr-3 pr-md-0 font-weight-bold'
+    for view in views:
+        dev = ("".join(re.findall(r"\w", view["title"])))  # <-- find only digits in "title" attribute
+    platform = soup.find('a', class_='font-weight-bold d-inline-block d-md-inline pb-3 pb-md-0 pr-3 pr-md-0').text
+    print(platform)
     try:
         banner_prince = soup.find('a', class_='btn btn-block text-uppercase btn-warning lh-1 ls-n05').text.strip('\n').strip('\n')
     except:
         banner_prince = 'no rating'
     videogames_names.append(name_and_console + '\n')
     videogames_description.append(description + '\n')
+    videogames_dev.append(dev + '\n')
+    #videogames_pub.append(pub + '\n')
     from nltk.stem import PorterStemmer
-
+#aggiungere sviluppatore, pubblicatore , woosh.
 
     ps = nltk.PorterStemmer()
     for w in videogames_description:
@@ -40,15 +53,17 @@ for link in videogames_links:
         g = open('/home/toore/gestione_dell_informazione/video_games_research/database/risultatiStamming.txt','a')
         g.write('%s\n' % rootWord)
         g.close()
-    print([nltk.word_tokenize(t) for t in videogames_names])
+    #print([nltk.word_tokenize(t) for t in videogames_names])
     games = {
-        'name_and_console':name_and_console,
+        'name_and_console': name_and_console,
         'description': description,
-        'banner_prince':banner_prince
+        'banner_prince': banner_prince,
+        'developer': dev,
+        #'publisher': pub
     }
    
     f = open('/home/toore/gestione_dell_informazione/video_games_research/database/risultati_scraping.csv', 'a')
     f.write('%s\n' % games)
     f.close()
-    #print(games)
+    print(games)
 
