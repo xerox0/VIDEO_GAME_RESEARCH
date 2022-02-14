09e2b75daf9939f27adbbfd05a39bdbef631d451
LMarco <lupis.marco@yahoo.it>
2022-02-14 09:51:41 +0100

    sistemazione dei risultati nella UI

--- UI/app.py
+++ UI/app.py
@@ -1,28 +1,24 @@
-from flask import Flask, render_template, request, redirect
-
+import sys
+sys.path.append("..")
+from flask import Flask, render_template, request
 from src import processing_query
-from processing_query import
+
 # configurazioni iniziali
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
-@app.route('/', methods=['POST', 'GET'])
-def home():  # put application's code here
-    # if request.method == 'POST':
-    #     task_content = request.form['content']  # è l'attributo name del form
-    #     #  qui passo i term inseriti alla funzione di ricerca
-    #     return redirect('/results.html')
-    # else:
-        return render_template('index.html')
-# dopo index.html ci va un'eventuale contenuto da passare alla pagina
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
-@app.route('/results.html', methods=['POST', 'GET'])
+@app.route('/results.html', methods=['GET', 'POST'])
 def risultati():
     query = {'text': request.args.get('generic'), 'developer': request.args.get('developer'),
              'platform': request.args.get('platform')}
-    list = process_query
+    print(query)
+    results_list = processing_query.process_query(query, 'multiplayer')
     """processare la query qui. mandarla ai due indici. Pseudocodice:
         x= process_query(query, 'instant_gaming') # ritorna i risultati della query. In process cercherà sia in instant gaming, sia in m
         multiplayer
@@ -30,8 +26,9 @@ def risultati():
         risultati = threshold (x,y)
         renderizza risultati a results.html
         """
+    print(results_list)
     # text_stemmed = create_index.preprocessing(query)
-    return render_template('results.html', data=list)
+    return render_template('results.html', data=results_list, query=query)
 
 
 if __name__ == '__main__':
